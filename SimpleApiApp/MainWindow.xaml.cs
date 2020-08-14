2102bc7814e56568c970f20a4d80becb5bde442a
Lauren <Lgrivier@hawkmail.hfcc.edu>
2020-08-13 20:21:49 -0400

    first push

---
+++ SimpleApiApp/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SimpleApiApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int maxNumber = 0;
+        private int currentNumber = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            ApiHelper.InitializeClient();
+            btnNext.IsEnabled = false;
+            
+        }
+
+        private async Task LoadImage(int imageNumber = 0)
+        {
+            var comic = await ComicProcessor.LoadComic(imageNumber);
+
+            if(imageNumber == 0)
+            {
+                maxNumber = comic.Num;
+            }
+
+            currentNumber = comic.Num;
+
+            var uriSource = new Uri(comic.Img, UriKind.Absolute);
+            comicImage.Source = new BitmapImage(uriSource);
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            await LoadImage();
+        }
+
+        private async void BtnPrevious_Click(object sender, RoutedEventArgs e)
+        {
+            if (currentNumber > 1)
+            {
+                currentNumber -= 1;
+                btnNext.IsEnabled = true;
+                await LoadImage(currentNumber);
+
+                if(currentNumber == 1)
+                {
+                    btnPrevious.IsEnabled = false;
+                }
+            }
+
+
+        }
+
+        private async void BtnNext_Click(object sender, RoutedEventArgs e)
+        {
+            if (currentNumber < maxNumber)
+            {
+                currentNumber += 1;
+                btnPrevious.IsEnabled = true;
+                await LoadImage(currentNumber);
+
+                if(currentNumber == maxNumber)
+                {
+                    btnNext.IsEnabled = false;
+                }
+            }
+        }
+    }
+}
